00368709ec44645d5d47250e46ab7ba8543c96d6
saurabh mishra <159324599+saurabhpbh450@users.noreply.github.com>
2024-10-27 20:15:36 +0530

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -23,7 +23,7 @@ const Login = () => {
       }else {
         // console.log(Values);
         const response = await axios.post(
-          "http://localhost:1000/api/v1/sign-in", 
+          "https://bookstore-backend-iits.onrender.com/api/v1/sign-in", 
           Values
         );
         dispatch(authActions.login());
