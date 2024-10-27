59e3023dadb1c99d7e0e98af46c3165b6d665f97
saurabh mishra <159324599+saurabhpbh450@users.noreply.github.com>
2024-10-27 20:17:33 +0530

    Update Profile.jsx

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -27,7 +27,7 @@ const Profile = () => {
       }
       try {
         const response = await axios.get(
-          'http://localhost:1000/api/v1/get-user-information',
+          'https://bookstore-backend-iits.onrender.com/api/v1/get-user-information',
           { headers }
         );
         setProfile(response.data);
